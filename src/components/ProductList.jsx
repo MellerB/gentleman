a913109b95c83bd080410113a7074ddc5a73f58f
MellerB <mellerbartomiej@gmail.com>
2021-01-10 17:01:47 +0100

    basic list of items done

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,26 +1,33 @@
-import React from "react"
-import {ReactComponent as CartIcon} from "../assets/shopping-cart.svg";  
+import React from "react"  
+import ProductListItem from "./ProductListItem";
 
 export default class ProductList extends React.Component {
     state={
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVYKaQl8WqoLowwFAYB417cJL7H__RG9oPg&usqp=CAU"
+        items:[
+            {
+                name: "Jacek Daniel",
+                age: 12,
+                type: "burbon",
+                origin: "USA",
+                price: 69.69,
+                imageURL: "https://propaganda24h.pl/environment/cache/images/500_500_productGfx_1293384820a33685a0938e2fd74f85c4.jpg"
+            },
+            {
+                name: "Jaś Wędrowniczek red",
+                age: 3,
+                type: "Blend",
+                origin: "chyba szkocja",
+                price: 3.50,
+                imageURL: "https://smaczajama.pl/userdata/public/gfx/a8f6bd71f68ed70a7adc9eacdaf5fa02.jpg"
+            },
+
+        ]
     }
 
-    render() {  
+    render() { 
       return (
       <div style={{height: "20px", width: "50%", margin: "auto"}}>
-        <div style={{height: "200px", margin: "10px", borderRadius: "5px", border: "solid black 2px", boxShadow: "5px 8px #f0f0f0a0"}}>
-        <img style={{float: "left", height: "100%", width: "15%", backgroundColor: "#f0f0f0", borderRight: "solid black 2px", objectFit: "cover"}} src={this.state.image} alt="noice whiskey"/> 
-        <div style={{paddingLeft: "5px",float: "left",height:"100%", width: "60%"}}>
-            <h2>'Nazwa łiski: string' 'int' lat</h2>
-            <h3>typ: 'string'</h3>
-            <h3>kraj pochodzenia: 'string'</h3>
-            <h3>cena: 'decimal' zł</h3>
-        </div>
-        <button style={{float: "right",height: "100%", width: "10%", backgroundColor: "#a0a0a0",boder:"solid black 2px", borderLeft: "solid black 2px"}}>
-        <CartIcon style={{width: "80%"}}/>
-        </button>
-        </div>
+          {this.state.items.map(item => <ProductListItem item={item}/>)}
       </div>
       );
     }
