61d396c0fb06096e459af2ebb90d64e6ca4f2f53
MichalMiekina <47924236+MichalMiekina@users.noreply.github.com>
2021-01-10 16:00:22 +0100

    main-page

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,8 +3,11 @@ import React from "react"
 export default class TopBar extends React.Component {
     render() {
       return (
-      <div style={{borderRadius: "5px", border: "black solid 2px", height: "20px", width: "100%"}}>
-        <button style={{float: "right"}}>KOSZYK</button>
+      <div style={{borderRadius: "5px", border: "black solid 2px", height: "40px", width: "100%", position: "absolute"}}>
+        <span>
+          Dzień Dobry
+        </span>
+        <img src={"../images/cart.jpg"} style={{float:"right"}}></img>
       </div>
       );
     }
