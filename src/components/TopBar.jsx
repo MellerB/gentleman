2120416be63a60cc614874398bb526f8a9758fc7
MichalMiekina <47924236+MichalMiekina@users.noreply.github.com>
2021-01-11 17:58:30 +0100

    limenu

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,14 +1,47 @@
 import React from "react"
+import {ReactComponent as CartIcon} from "../assets/shopping-cart.svg";
+import {ReactComponent as GentlemanIcon} from "../assets/gentleman-icon.svg";
+
+// var menu_button_width = document.getElementById("cart-menu").style.width;
+
+
 
 export default class TopBar extends React.Component {
     render() {
       return (
-      <div style={{borderRadius: "5px", border: "black solid 2px", height: "40px", width: "100%", position: "absolute"}}>
-        <span>
-          Dzień Dobry
-        </span>
-        <img src={"../images/cart.jpg"} style={{float:"right"}}></img>
+      <div style={{height: "60px", width: "100%", position: "fixed",backgroundColor: "#dddddd",margin: "auto",top:0}}>
+        
+
+        <ul id="menu-bar" style={{}}>
+
+          <li>
+            <span>
+              Dzień Dobry
+            </span>
+          </li>
+          <li>
+            <a href="#" id="menu-logo" style={{height: "100%", backgroundColor: "#a0a0a0",boder:"solid black 2px", borderLeft: "solid black 2px"}}>
+              <div>
+                {/* <GentlemanIcon style={{height: "100%",display:"block"}}/> */}
+                <img src="../images/cart.jpg" ></img>
+              </div>
+              
+            </a>
+          </li>
+          <li>
+            <a href="#" id="menu-cart" style={{height: "100%", backgroundColor: "#a0a0a0",boder:"solid black 2px", borderLeft: "solid black 2px"}}>
+              <div>
+                <CartIcon style={{height: "100%",display:"block"}}/>
+              </div>
+              
+            </a>
+          </li>
+        </ul>
+        
+
+        
       </div>
       );
     }
 }
+
