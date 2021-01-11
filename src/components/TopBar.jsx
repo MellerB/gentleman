f9bfbd57a6afbe722ecf99e07c009e81fdadf54a
MichalMiekina <47924236+MichalMiekina@users.noreply.github.com>
2021-01-11 21:41:54 +0100

    headermenunav

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -9,83 +9,84 @@ import {ReactComponent as GentlemanIcon} from "../assets/gentleman-icon.svg";
 export default class TopBar extends React.Component {
     render() {
       return (
-      <nav style={{height: "60px", width: "100%", position: "fixed",backgroundColor: "#dddddd",margin: "auto",top:0}}>
-        
+        <header>
+          <nav style={{height: "60px", width: "100%", position: "fixed",backgroundColor: "#dddddd",margin: "auto",top:0}}>
+            <ul class="left-menu-bar" style={{}}>
 
-        <ul class="left-menu-bar" style={{}}>
-
-          <li>
-            <a href="#" id="menu-logo" style={{height: "100%"}}>
-              <div>
-                Home
+              <li>
+                <a href="#" id="menu-logo" style={{height: "100%"}}>
+                  <div>
+                    Home
+                    {/* <GentlemanIcon style={{height: "100%",display:"block"}}/> */}
+                    {/* <img src="../images/cart.jpg" ></img> */}
+                  </div>
+                  
+                </a>
+              </li>
+              <li>
+                <a href="#" id="menu-cart" style={{height: "100%"}}>
+                  <div>
+                    Dzień
+                    {/* <CartIcon style={{height: "100%",display:"block"}}/> */}
+                  </div>
+                  
+                </a>
+              </li>
+              <li>
+                <a href="#" id="menu-cart" style={{height: "100%"}}>
+                  <div>
+                    Dobry
+                    {/* <CartIcon style={{height: "100%",display:"block"}}/> */}
+                  </div>
+                  
+                </a>
+              </li>
+            </ul>
+            <ul class="logo-menu-bar">
+              <li>
+                <a href="#">
                 {/* <GentlemanIcon style={{height: "100%",display:"block"}}/> */}
-                {/* <img src="../images/cart.jpg" ></img> */}
-              </div>
-              
-            </a>
-          </li>
-          <li>
-            <a href="#" id="menu-cart" style={{height: "100%"}}>
-              <div>
-                Dzień
-                {/* <CartIcon style={{height: "100%",display:"block"}}/> */}
-              </div>
-              
-            </a>
-          </li>
-          <li>
-            <a href="#" id="menu-cart" style={{height: "100%"}}>
-              <div>
-                Dobry
-                {/* <CartIcon style={{height: "100%",display:"block"}}/> */}
-              </div>
-              
-            </a>
-          </li>
-        </ul>
-        <ul class="logo-menu-bar">
-          <li>
-            <a href="#">
-            {/* <GentlemanIcon style={{height: "100%",display:"block"}}/> */}
-            <CartIcon style={{height: "100%",display:"block"}}/>
-            </a>
-          </li>
-        </ul>
-        <ul class="right-menu-bar" style={{}}>
+                <CartIcon style={{height: "100%",display:"block"}}/>
+                </a>
+              </li>
+            </ul>
+            <ul class="right-menu-bar" style={{}}>
 
-          <li>
-            <a href="#" id="menu-logo" style={{height: "100%"}}>
-              <div>
-                Wieczór
-                {/* <GentlemanIcon style={{height: "100%",display:"block"}}/> */}
-                {/* <img src="../images/cart.jpg" ></img> */}
-              </div>
+              <li>
+                <a href="#" id="menu-logo" style={{height: "100%"}}>
+                  <div>
+                    Wieczór
+                    {/* <GentlemanIcon style={{height: "100%",display:"block"}}/> */}
+                    {/* <img src="../images/cart.jpg" ></img> */}
+                  </div>
+                  
+                </a>
+              </li>
               
-            </a>
-          </li>
-          
-          <li>
-            <a href="#" id="menu-cart" style={{height: "100%"}}>
-              <div>
-                Kontakt
-                {/* <CartIcon style={{height: "100%",display:"block"}}/> */}
-              </div>
-              
-            </a>
-          </li>
+              <li>
+                <a href="#" id="menu-cart" style={{height: "100%"}}>
+                  <div>
+                    Kontakt
+                    {/* <CartIcon style={{height: "100%",display:"block"}}/> */}
+                  </div>
+                  
+                </a>
+              </li>
 
-          <li>
-            <a href="#" id="menu-cart" style={{height: "100%"}}>
-              <div>
-              <CartIcon style={{height: "100%",display:"block"}}/>
-                {/* <CartIcon style={{height: "100%",display:"block"}}/> */}
-              </div>
-              
-            </a>
-          </li>
-          </ul>
-        
-      </nav>
+              <li>
+                <a href="#" id="menu-cart" style={{height: "100%"}}>
+                  <div>
+                  <CartIcon style={{height: "100%",display:"block"}}/>
+                    {/* <CartIcon style={{height: "100%",display:"block"}}/> */}
+                  </div>
+                  
+                </a>
+              </li>
+              </ul>
+            
+          </nav>
+        </header>
+      
       );
     }
 }
