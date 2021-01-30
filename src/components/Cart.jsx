3b1ec773972ebad712db7abc54470af18b434f0c
MellerB <mellerbartomiej@gmail.com>
2021-01-30 12:15:57 +0100

    merge menubar

---
+++ src/components/Cart.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default class Cart extends React.Component {
+
+    render() { 
+      return (
+      <div style={{height: "200px", width: "200px", background: "red"}}>
+          
+      </div>
+      );
+    }
+}
--- src/components/NavListItem.jsx
+++ src/components/NavListItem.jsx
@@ -1,4 +1,8 @@
 import React from "react"
+import {ReactComponent as CartIcon} from "../assets/shopping-cart.svg";
+import {ReactComponent as GentlemanIcon} from "../assets/gentleman-icon.svg";
+import GentlemanLogo from "../images/gentleman-logo.png";
+import CartIconJPG from "../images/cart.jpg";
 
 // var menu_button_width = document.getElementById("cart-menu").style.width;
 
@@ -11,7 +15,6 @@ export default class NavListItem extends React.Component {
 <a href="#" style={{height: "100%"}}>
   <div>
     {this.props.text}
-    {/* {this.props.children} */}
 </div>
 </a>
 </li>
