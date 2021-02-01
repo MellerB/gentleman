94475c606fb8306495ab2cdade77a9e1f4617c47
MellerB <mellerbartomiej@gmail.com>
2021-02-01 16:34:25 +0100

    refactor

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,29 +1,29 @@
 import React from "react"
 
 export default class Main extends React.Component {
-
   render() {
+
+
+    const authors = [
+      {name: "Michał Miękina", mail:"michmie178@student.polsl.pl"},
+      {name: "Bartłomiej Meller", mail:"bartmel655@student.polsl.pl"}
+    ]
     return (
       <div style={{ width: "60%", margin: "auto", border: "solid #C7A82B 2px",padding:"0 0 15px 15px",backgroundColor:"#eeeedf" }}>
-        <div style={{}}>
-          <h4>Obsługa klienta</h4>
-          <a href="mailto:gentlemanbok@gntl.pl">gentlemanbok@gntl.pl</a><br/>
-          32 54 69 130<br/>
-          32 54 69 131
-          
-        </div>
         <div>
-          <h4>Współpraca</h4>
-          <a href="mailto:gentlemancoop@gntl.pl">gentlemancoop@gntl.pl</a><br/>
-          32 54 69 132<br/>
-          507328443
-        </div>
-        <div>
-          <h4>Lokalizacja</h4>
-          <a href="https://goo.gl/maps/SBFnVFuDijPt3f5d9" target="_blank" rel="noreferrer">
-          Gliwice 44-100, śląskie<br/>
-          ul. Kaszubska 23
-          </a>
+          <h4>Autorzy:</h4>
+          <div style={{marginLeft: "10px", marginTop: "-10px"}}>
+          {authors.map(author => <span>
+            {author.name}<br/>
+            <a style={{color: "black"}} href={"mailto:"+author.mail}>{author.mail}</a><br/>
+            <br/>
+            </span>)}
+            </div>
+            <h4>Przedmiot:</h4>
+            <div style={{marginLeft: "10px", marginTop: "-10px"}}>
+            "Aplikacje Webowe"<br/>
+            </div>
+            <br/>
         </div>
       </div>
     );
