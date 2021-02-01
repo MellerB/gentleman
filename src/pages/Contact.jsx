1f4ac2c4c5d3e0ec84461a946ddb6d7599286005
MichalMiekina <47924236+MichalMiekina@users.noreply.github.com>
2021-02-01 14:43:10 +0100

    contact content

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,11 +2,30 @@ import React from "react"
 
 export default class Main extends React.Component {
 
-    render() { 
-      return (
-        <div style={{height: "20px", width: "50%", margin: "auto"}}>
-          XD
+  render() {
+    return (
+      <div style={{ width: "60%", margin: "auto", border: "solid #C7A82B 2px",padding:"0 0 15px 15px",backgroundColor:"#eeeedf" }}>
+        <div style={{}}>
+          <h4>Obsługa klienta</h4>
+          <a href="mailto:gentlemanbok@gntl.pl">gentlemanbok@gntl.pl</a><br/>
+          32 54 69 130<br/>
+          32 54 69 131
+          
+        </div>
+        <div>
+          <h4>Współpraca</h4>
+          <a href="mailto:gentlemancoop@gntl.pl">gentlemancoop@gntl.pl</a><br/>
+          32 54 69 132<br/>
+          507328443
+        </div>
+        <div>
+          <h4>Lokalizacja</h4>
+          <a href="https://goo.gl/maps/SBFnVFuDijPt3f5d9" target="_blank" rel="noreferrer">
+          Gliwice 44-100, śląskie<br/>
+          ul. Kaszubska 23
+          </a>
+        </div>
       </div>
-      );
-    }
+    );
+  }
 }
